98449c2821b52054dc9f9613e8a6ee3a8e6d9b0d
Cepexx <82107568+Cepexx@users.noreply.github.com>
2021-12-15 09:55:31 +0100

    01- creación de la estructura, atributos y constructor de la segunda clase

---
+++ Procesador.java
@@ -0,0 +1,20 @@
+
+/**
+ * Clase Procesador
+ */
+public class Procesador
+{
+    // instance variables - replace the example below with your own
+    private int voltaje;
+    private boolean graficaIntegrada;
+
+    /**
+     * Constructor para la clase Procesador
+     */
+    public Procesador(int voltajeProcesador, boolean graficaIntegradaProcesador)
+    {
+        // Inicializa las variables
+        voltaje = voltajeProcesador;
+        graficaIntegrada = graficaIntegradaProcesador;
+    }
+}
